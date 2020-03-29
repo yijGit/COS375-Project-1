525cf3d0080c895073487a528c816c6e113a1fbb
Alexander Helman <ahelman@princeton.edu>
2020-03-29 14:21:02 -0700

    sim skeleton code

--- Project1/src/sim.cpp
+++ Project1/src/sim.cpp
@@ -1,27 +1,46 @@
-main(argc, argv) {
+#include <iostream>
+#include <iomanip>
+#include "MemoryStore.h"
+#include "RegisterInfo.h"
+
+using namespace std;
+
+int main(argc, argv)
+{
   // Create	a	memory	store	called	myMem
+  MemoryStore *myMem = createMemoryStore();
   // Initialize	registers	to	have	value	0
+  RegisterInfo reg;
   // Read	bytes	of	binary	file passed	as	parameter into appropriate	memory	locations
+
   // Point	the	program	counter	to	the	first	instruction
-  while (TRUE) {
+  while (TRUE)
+  {
     // Fetch	current	instruction	from	memory@PC
     // Determine	the	instruction	type
     // Get	the	operands
-    switch (instruction	type)	{
-    case 0xfeedfeed:
-      // RegisterInfo	reg;
-      // Fill	reg	with	the	current	contents	of	the	registers
-      dumpRegisterState(reg);
-      dumpMemoryState(myMem);
-      return 0;
-    case INSTR1:
-      // Perform	operation	and	update	destination
-      // register/memory/PC
-      break;
-    // ...
-    default:
-      fprintf(stderr, ”Illegal	operation...”);
-      exit(127);
+    switch (instruction	type)
+    {
+      case 0xfeedfeed:
+        // RegisterInfo	reg;
+        // Fill	reg	with	the	current	contents	of	the	registers
+        dumpRegisterState(reg);
+        dumpMemoryState(myMem);
+        return 0;
+      case INSTR1:
+        // Perform	operation	and	update	destination
+        // register/memory/PC
+        break;
+      // ...
+      default:
+        fprintf(stderr, ”Illegal	operation...”);
+        exit(127);
     }
   }
+  
+  dumpRegisterState(reg);
+  dumpMemoryState(myMem);
+
+  delete myMem;
+  return 0;
 }
